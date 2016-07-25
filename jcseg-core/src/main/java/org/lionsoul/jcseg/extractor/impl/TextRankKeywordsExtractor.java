7c65fd5ef43bb452e92e3851d3413205fbbcbb39
Black-Chen <cda332978465@gmail.com>
2016-07-25 16:28:58 +0800

    计算关键字时，加入权重
    1.计算关键字时，加入了权重的计算
    2.计算关键字时，返回值中加入了分数

--- jcseg-core/src/main/java/org/lionsoul/jcseg/extractor/impl/TextRankKeywordsExtractor.java
+++ jcseg-core/src/main/java/org/lionsoul/jcseg/extractor/impl/TextRankKeywordsExtractor.java
@@ -2,6 +2,7 @@ package org.lionsoul.jcseg.extractor.impl;
 
 import java.io.IOException;
 import java.io.Reader;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -43,73 +44,70 @@ public class TextRankKeywordsExtractor extends KeywordsExtractor
         super(seg);
     }
 
+    /**
+     * get the keywords list from a string
+     *
+     * @param    doc
+     * @return  List[]
+     * @throws    IOException
+     */
+    public List<String> getKeywordsFromString(String doc, Integer weight) throws IOException
+    {
+        return getKeywords(new StringReader(doc), weight);
+    }
+
+    public List<String> getKeywords(Reader reader, Integer weight) throws IOException {
+
+        Map<String, List<String>> winMap = new HashMap<String, List<String>>();
+        List<String> words = new ArrayList<String>();
+        HashMap<String, Float> score = textRankCalc(reader, winMap, words);
+
+        //sort the items by score
+        List<Map.Entry<String, Float>> entryList = new ArrayList<Map.Entry<String, Float>>(score.entrySet());
+        Collections.sort(entryList, new Comparator<Map.Entry<String, Float>>(){
+            @Override
+            public int compare(Map.Entry<String, Float> o1,
+                    Map.Entry<String, Float> o2) {
+                return o2.getValue().compareTo(o1.getValue());
+            }
+        });
+
+        float tScores = 0F, avgScores = 0F, stdScores = 0F;
+        for ( Map.Entry<String, Float> entry : entryList )
+        {
+            tScores += entry.getValue();
+            //System.out.println(entry.getKey()+"="+entry.getValue());
+        }
+
+        avgScores = tScores / words.size();
+        stdScores = avgScores * (1 + D);
+
+        //return the sublist as the final result
+        int len = Math.min(keywordsNum, entryList.size());
+        List<String> keywords = new ArrayList<String>(len);
+        for ( int i = 0; i < entryList.size(); i++ )
+        {
+            Map.Entry<String, Float> e = entryList.get(i);
+            if ( i >= len ) break;
+            if ( autoFilter && e.getValue() < stdScores ) break;
+            keywords.add(e.getKey() + "_" + (e.getValue() * weight));
+        }
+
+        //let gc do its work
+        winMap.clear();
+        words.clear();
+        winMap = null; words = null;
+        score = null; entryList = null;
+
+        return keywords;
+    }
+
     @Override
     public List<String> getKeywords(Reader reader) throws IOException 
     {
         Map<String, List<String>> winMap = new HashMap<String, List<String>>();
         List<String> words = new ArrayList<String>();
-        
-        //document segment
-        IWord w = null;
-        seg.reset(reader);
-        while ( (w = seg.next()) != null )
-        {
-            if ( filter(w) == false ) continue;
-            
-            String word = w.getValue();
-            if ( ! winMap.containsKey(word) ) 
-            {
-                winMap.put(word, new LinkedList<String>());
-            }
-            
-            words.add(word);
-        }
-        
-        //count the neighbour
-        for ( int i = 0; i < words.size(); i++ )
-        {
-            String word = words.get(i);
-            List<String> support = winMap.get(word);
-            
-            int sIdx = Math.max(0, i - windowSize);
-            int eIdx = Math.min(i + windowSize, words.size() - 1);
-            
-            for ( int j = sIdx; j <= eIdx; j++ )
-            {
-                if ( j == i ) continue;
-                support.add(words.get(j));
-            }
-        }
-        
-        //do the page rank scores caculate
-        HashMap<String, Float> score = new HashMap<String, Float>();
-        for ( int c = 0; c < maxIterateNum; c++ )
-        {
-            for ( Map.Entry<String, List<String>> entry : winMap.entrySet() )
-            {
-                String key = entry.getKey();
-                List<String> value = entry.getValue();
-                
-                float sigema = 0F;
-                for ( String ele : value )
-                {
-                    int size = winMap.get(ele).size();
-                    if ( ele.equals(key) || size == 0 ) {
-                        continue;
-                    }
-                    
-                    float Sy = 0;
-                    if ( score != null 
-                            && score.containsKey(ele) ) {
-                        Sy = score.get(ele);
-                    }
-                    
-                    sigema += Sy / size;
-                }
-                
-                score.put(key, 1 - D + D * sigema);
-            }
-        }
+        HashMap<String, Float> score = textRankCalc(reader, winMap, words);
 
         //sort the items by score
         List<Map.Entry<String, Float>> entryList = new ArrayList<Map.Entry<String, Float>>(score.entrySet());
@@ -139,7 +137,7 @@ public class TextRankKeywordsExtractor extends KeywordsExtractor
             Map.Entry<String, Float> e = entryList.get(i);
             if ( i >= len ) break;
             if ( autoFilter && e.getValue() < stdScores ) break;
-            keywords.add(e.getKey());
+            keywords.add(e.getKey() + "_" + e.getValue());
         }
         
         //let gc do its work
@@ -149,6 +147,71 @@ public class TextRankKeywordsExtractor extends KeywordsExtractor
         score = null; entryList = null;
         
         return keywords;
+    }
+
+    private HashMap<String, Float> textRankCalc(Reader reader, Map<String, List<String>> winMap, List<String> words) throws IOException {
+        //document segment
+        IWord w = null;
+        seg.reset(reader);
+        while ( (w = seg.next()) != null )
+        {
+            if ( filter(w) == false ) continue;
+
+            String word = w.getValue();
+            if ( ! winMap.containsKey(word) )
+            {
+                winMap.put(word, new LinkedList<String>());
+            }
+
+            words.add(word);
+        }
+
+        //count the neighbour
+        for ( int i = 0; i < words.size(); i++ )
+        {
+            String word = words.get(i);
+            List<String> support = winMap.get(word);
+
+            int sIdx = Math.max(0, i - windowSize);
+            int eIdx = Math.min(i + windowSize, words.size() - 1);
+
+            for ( int j = sIdx; j <= eIdx; j++ )
+            {
+                if ( j == i ) continue;
+                support.add(words.get(j));
+            }
+        }
+
+        //do the page rank scores caculate
+        HashMap<String, Float> score = new HashMap<String, Float>();
+        for ( int c = 0; c < maxIterateNum; c++ )
+        {
+            for ( Map.Entry<String, List<String>> entry : winMap.entrySet() )
+            {
+                String key = entry.getKey();
+                List<String> value = entry.getValue();
+
+                float sigema = 0F;
+                for ( String ele : value )
+                {
+                    int size = winMap.get(ele).size();
+                    if ( ele.equals(key) || size == 0 ) {
+                        continue;
+                    }
+
+                    float Sy = 0;
+                    if ( score != null
+                            && score.containsKey(ele) ) {
+                        Sy = score.get(ele);
+                    }
+
+                    sigema += Sy / size;
+                }
+
+                score.put(key, 1 - D + D * sigema);
+            }
+        }
+        return score;
     }
 
     public int getKeywordsNum() {
